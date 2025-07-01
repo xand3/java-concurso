d60fdbfee392d881ca72b73a7b5a53d10af97b48
Alexandre Bastos <xande.dev@outlook.com>
2025-06-30 21:14:49 -0300

    exercicios estrutura sequencial

--- src/exercicios/estrutura_sequencial/exercicio6.java
+++ src/exercicios/estrutura_sequencial/exercicio6.java
@@ -1,2 +1,26 @@
-package exercicios.estrutura_sequencial;public class exercicio6 {
+package exercicios.estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class exercicio6 {
+    public static void main(String[] args) {
+        double a, b, c, areaTriangulo, areaCirculo, areaTrapezio, areaQuadrado, areaRetangulo, pi = 3.14159;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+        areaTriangulo = (a * c) / 2;
+        areaCirculo = pi * (c * c);
+        areaTrapezio = ((a + b) * c) / 2;
+        areaQuadrado = b * b;
+        areaRetangulo = a * b;
+
+        System.out.printf("TRIANGULO: %.3f%n" +
+                "CIRCULO: %.3f%n" +
+                "TRAPEZIO: %.3f%n" +
+                "QUADRADO: %.3f%n" +
+                "RETANGULO: %.3f%n", areaTriangulo, areaCirculo, areaTrapezio, areaQuadrado, areaRetangulo);
+
+        sc.close();
+    }
 }

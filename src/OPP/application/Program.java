03228416e35c70b5a6980f2fce23eaf268b58e99
Alexandre Bastos <xande.dev@outlook.com>
2025-07-07 21:34:34 -0300

    :notebook: Iniciando POO

---
+++ src/OPP/application/Program.java
@@ -0,0 +1,41 @@
+package OPP.application;
+
+import OPP.application.entities.Triangle;
+
+import java.util.Scanner;
+// PROGRAMA SEM UTILIZAR POO
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double p, areaX, areaY;
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Digite as medidas do triangulo X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+
+        System.out.println("Digite as medidas do triangulo Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        areaX = x.area();
+        areaY = y.area();
+
+        System.out.printf("Area do triangulo X: %.4f%n", areaX);
+        System.out.printf("Area do triangulo Y: %.4f%n", areaY);
+        if( areaX > areaY){
+            System.out.println("Area maior: X");
+        } if(areaX < areaY) {
+            System.out.println("Area maior: Y");
+        } else {
+            System.out.println("Areas iguais");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/questoes_de_prova/questao50.java
@@ -0,0 +1,7 @@
+package questoes_de_prova;
+
+public class questao50 {
+    public class val1 {
+        
+    }
+}
